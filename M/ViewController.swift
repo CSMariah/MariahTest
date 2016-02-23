c2b11f2cda4acaa7d7ee9be6172fe18d2d560395
Mariah KH <Mariah@mariahs-macbook-pro.local>
2016-02-23 15:59:36 +0300

    Initial Commit

---
+++ M/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M
+//
+//  Created by Mariah Sami Khayat on 2/23/16.
+//  Copyright © 2016 Mariah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
